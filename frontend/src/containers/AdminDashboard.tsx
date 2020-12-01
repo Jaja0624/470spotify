4d49bab368ff65a5e99722602686a21288b907fe
Maple Tan <maplet@sfu.ca>
2020-11-30 16:41:22 -0800

    Add tabs for switching between db tables

--- frontend/src/containers/AdminDashboard.tsx
+++ frontend/src/containers/AdminDashboard.tsx
@@ -2,15 +2,43 @@ import React, {useEffect, useState} from 'react';
 import { RouteComponentProps, withRouter} from 'react-router-dom';
 import { makeStyles, Theme, createStyles} from '@material-ui/core/styles'
 import AdminMainAppBar from '../components/AdminMainAppBar'
+import { Tab, Tabs } from '@material-ui/core';
+import globalStore from '../store/global';
+import userStore from '../store/user';
 
 interface Props extends RouteComponentProps {}
 
 const AdminDashboard: React.FC<Props> = ({history}) => {
+    const globalState = globalStore();
+    const userState = userStore();
     const classes = useStyles();
+
+    const handleChange = (event : any, newValue: number) => {
+        globalState.setAdminTabIndex(newValue);
+    };
+
+    const adminTableComponentShow = () => {
+        // TODO: populate tables for each DB table
+    }
 
     return (
         <div className={classes.root}>
             <AdminMainAppBar/>
+            <br/>
+            <br/>
+            <br/>
+            <div>
+                <Tabs value={globalState.adminTabIndex} onChange={handleChange} aria-label="simple tabs example" variant="fullWidth"
+                    scrollButtons="on" >
+                    <Tab label="AppUser"/>
+                    <Tab label="AppGroup"/>
+                    <Tab label="GroupMember"/>
+                    <Tab label="AppSession"/>
+                    <Tab label="SessionAdmin"/>
+                    <Tab label="AppHistory"/>
+                </Tabs>
+                {adminTableComponentShow()}
+            </div>
         </div>
     );
 }

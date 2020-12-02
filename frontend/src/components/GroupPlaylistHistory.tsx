b3067d5c467ef2f1bf7185f84e853227139693cf
Miguel <jmigst@gmail.com>
2020-12-02 11:19:33 -0800

    Use get group history session playlist functions in GroupPlaylistHistory. Handle case where there is no history session playlist in ViewSessionPlaylistHIstoryModal.

--- frontend/src/components/GroupPlaylistHistory.tsx
+++ frontend/src/components/GroupPlaylistHistory.tsx
@@ -6,112 +6,27 @@ import { List } from '@material-ui/core';
 import GroupPlaylistHistoryItem from '../components/GroupPlaylistHistoryItem';
 import ViewSessionPlaylistHistoryModal from './ViewSessionPlaylistHistoryModal';
 import SessionPlaylist from '../types/SessionPlaylist';
+import userStore from '../store/user';
+import { getHistorySessionPlaylists } from '../core/server';
 
 //Structure of the props for this component
 interface Props extends RouteComponentProps{}
 
-//TODO: Temporary, remove when properly retrieving session playlist data.
-const examplePlaylistArray : Array<SessionPlaylist> = [
-    {
-        session_uid: 1,
-        start_date: new Date(),
-        participants: ["samplePerson1", "samplePerson2"],
-        songs: [
-            {
-                date_added: new Date(),
-                app_user: "samplePerson1",
-                songname : "song1",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson2",
-                songname : "song2",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            }
-        ]
-    },
-    {
-        session_uid: 2,
-        start_date: new Date(),
-        participants: ["samplePerson3", "samplePerson4"],
-        songs: [
-            {
-                date_added: new Date(),
-                app_user: "samplePerson3",
-                songname : "song3",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            },
-            {
-                date_added: new Date(),
-                app_user: "samplePerson4",
-                songname : "song4",
-                song_uri : "someSonguri",
-                group_name: "somegroup1"
-            }
-        ]
-    }
-];
 
 //Shows the history of a group as sessions, represented as a list of past sessions.
 //When a list item is chosen, it opens up a modal that allows the user to view the
 //songs and potentially export it.
 const GroupPlaylistHistory: React.FC<Props> = () => {
     //Determines what information to show on the playlist history.
+    const userState = userStore();
 
     //Assumes the element of a playlist is structured as a Playlist. The playlist
     //is set by any changes from the server.
-    //TODO: replace for real data, probably in user store
-    const [sessionPlaylistHistory, setPlaylistHistory] = useState<Array<SessionPlaylist>>(examplePlaylistArray);
+    const [sessionPlaylistHistory, setPlaylistHistory] = useState<Array<SessionPlaylist>>([]);
 
     //State for the modal that looks at the current playlist. The modal reads the
     //playlist, the GroupPlaylistHistoryItem sets the current playlist.
-    const [currentPlaylist, setPlaylist] = useState<SessionPlaylist>(sessionPlaylistHistory[0]);
+    const [currentPlaylist, setPlaylist] = useState<SessionPlaylist | undefined>(undefined);
 
     //State of the visibility of the modal component.
     const [isModalVisible, setIsModalVisible] = useState(false);
@@ -127,6 +42,29 @@ const GroupPlaylistHistory: React.FC<Props> = () => {
         setPlaylist(playlist);
         setIsModalVisible(true);
     };
+
+    async function pullPlaylists() {
+        if (userState?.currentGroup?.id) {
+            console.log("<GroupPlaylistHistory>: currentgroup = ", userState.currentGroup?.id);
+            const backendPlaylists = await getHistorySessionPlaylists(parseInt(userState.currentGroup?.id));
+            console.log("<GroupPlaylistHistory>: New history session playlist data taken from the backend: ", backendPlaylists);
+            if (backendPlaylists.status === 200) {
+                setPlaylistHistory(backendPlaylists.data);
+            } else {
+                console.log("error getting playlist", backendPlaylists);
+            }
+        } else {
+            console.log(`<GroupPlaylistHistory>: The current group might be null: ${userState?.currentGroup?.id}`);
+        }
+    }
+
+    useEffect(() => {
+        console.log(`<GroupPlaylistHistory>: The current group has changed to id: ${userState.currentGroup?.id}. Updating...`);
+        async function scopedPull() {
+            await pullPlaylists();
+        }
+        scopedPull();
+    }, [userState.currentGroup])
 
     return (
         <div>

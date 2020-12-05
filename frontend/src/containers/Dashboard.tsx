be1dec3e4dd22eb41b24984a72a0cdf8709c15bf
Josh Baltar <joshbaltar@gmail.com>
2020-12-04 20:29:44 -0800

    scroll bar fixed

--- frontend/src/containers/Dashboard.tsx
+++ frontend/src/containers/Dashboard.tsx
@@ -134,20 +134,20 @@ const useStyles = makeStyles((theme: Theme) =>
         float:'left',
         textAlign:'left'
       },
-      '@global': {
-            '*::-webkit-scrollbar': {
-            width: '10px'
-            },
-            '*::-webkit-scrollbar-track': {
-                '-webkit-box-shadow': 'inset 0 0 6px rgba(35, 39, 42, .7)',
-                '-webkit-border-radius': '10px'
-            },
-            '*::-webkit-scrollbar-thumb': {
-                '-webkit-border-radius': '10px',
-                'border-radius': '10px',
-                'background': 'rgba(44, 47, 51)' 
-            }
+    '@global': {
+        '*::-webkit-scrollbar': {
+        width: '10px'
+        },
+        '*::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(35, 39, 42, .7)',
+            '-webkit-border-radius': '10px'
+        },
+        '*::-webkit-scrollbar-thumb': {
+            '-webkit-border-radius': '10px',
+            'border-radius': '10px',
+            'background': 'rgba(44, 47, 51)' 
         }
+    }
   }),
 );
 

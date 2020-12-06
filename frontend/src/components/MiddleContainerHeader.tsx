583c9e1f116adc1ed3a1980e946d3293f33ce40b
Miguel <jmigst@gmail.com>
2020-12-05 16:26:43 -0800

    Add if statement on MiddleContainerHeader.

--- frontend/src/components/MiddleContainerHeader.tsx
+++ frontend/src/components/MiddleContainerHeader.tsx
@@ -3,6 +3,7 @@ import globalStore from '../store/global'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { Button, Typography} from '@material-ui/core'
 import { RouteComponentProps, withRouter} from 'react-router-dom';
+import userStore from '../store/user';
 
 
 // extending RouteComponentProps allow us to bring in prop types already declared in RouteComponentProps
@@ -12,15 +13,22 @@ interface CustomPropsLol extends RouteComponentProps {}
 const MiddleContainerHeader: React.FC<CustomPropsLol> = ({history}: CustomPropsLol) => {
     const classes = useStyles();
     const globalState = globalStore();
+    const userState = userStore();
+
+    const getSessionHeader = () => {
+        if(userState.currentSessionData && userState.currentSessionData.playlist){
+            return (<Button color={globalState.middleContainer != "group" ? 'primary' : 'default'} onClick={() => globalState.setMiddleContainer('session')}>
+            <Typography style={{fontWeight:'bold'}}>Active Session Page</Typography>
+        </Button>)
+        }
+    };
 
     return (
         <div className={classes.root}>
             <Button color={globalState.middleContainer == "group" ? 'primary' : 'default'} onClick={() => globalState.setMiddleContainer('group')}>
                 <Typography style={{fontWeight:'bold'}}>Group Page</Typography>
             </Button>
-            <Button color={globalState.middleContainer != "group" ? 'primary' : 'default'} onClick={() => globalState.setMiddleContainer('session')}>
-                <Typography style={{fontWeight:'bold'}}>Session If Active (Disable if no session active)</Typography>
-            </Button>
+            {getSessionHeader()}
         </div>
     )
 }
